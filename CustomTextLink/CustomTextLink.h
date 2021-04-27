2c2bbfac273bb9c27134f90813601662df4ae66d
Shreyanka B Honnalli <shreyankab_h@BGMB128330.local>
2021-04-27 07:54:25 +0530

    Initial Project Setup

---
+++ CustomTextLink/CustomTextLink.h
@@ -0,0 +1,18 @@
+//
+//  CustomTextLink.h
+//  CustomTextLink
+//
+//  Created by Shreyanka B Honnalli on 27/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomTextLink.
+FOUNDATION_EXPORT double CustomTextLinkVersionNumber;
+
+//! Project version string for CustomTextLink.
+FOUNDATION_EXPORT const unsigned char CustomTextLinkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomTextLink/PublicHeader.h>
+
+
---
+++ CustomtextLinkExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomtextLinkExamples
+//
+//  Created by Shreyanka B Honnalli on 27/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
